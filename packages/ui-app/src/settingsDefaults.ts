caf88b158bee8f8f7a99f1c34cddba91b0d025ae
Bryan Chen <bryan.chen@centrality.ai>
2018-11-22 15:18:21 +1300

    only keep dev endpoint

--- packages/ui-app/src/settingsDefaults.ts
+++ packages/ui-app/src/settingsDefaults.ts
@@ -21,24 +21,10 @@ const CHAINS: ChainsInfo = [
     chainId: 0,
     decimals: 0,
     unit: 'Unit'
-  },
-  {
-    name: 'Local Testnet',
-    chainId: 0,
-    decimals: 0,
-    unit: 'Unit'
-  },
-  {
-    name: 'BBQ Birch',
-    chainId: 68,
-    decimals: 15,
-    unit: 'BBQ'
   }
 ];
 
 const ENDPOINTS: Options = [
-  { text: 'BBQ Birch (hosted by Parity)', value: 'wss://substrate-rpc.parity.io/' },
-  { disabled: true, text: 'Polkadot PoC-3 (hosted by Parity)', value: 'wss://polkadot-rpc.polkadot.io/' },
   { text: 'Local Node (127.0.0.1:9944)', value: 'ws://127.0.0.1:9944/' }
 ];
 
